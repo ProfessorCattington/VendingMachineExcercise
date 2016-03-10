cdbbf132c8d3f03f909467ce027f771d004317bb
Michael Lezon <mister.catsby@gmail.com>
2016-03-10 15:58:25 -0500

    added a test to make sure the display sold out strategy changes the message after a short wait.

--- VendingMachine/VendingMachineTest/DisplaySoldOutStrategyTests.cs
+++ VendingMachine/VendingMachineTest/DisplaySoldOutStrategyTests.cs
@@ -22,5 +22,36 @@ namespace VendingMachineTestNS {
 
             Assert.AreEqual(testSoldOut, digitalDisplay.GetCurrentMessage());
         }
+
+        [TestMethod]
+        public void TestSoldOutStrategyChangesMessageAfterAShortWait(){
+
+            testVendingMachineController = new VendingMachineController();
+
+            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
+            digitalDisplay.UserSelectedASoldOutProduct();
+
+            CoinAccepter coinAccepter = testVendingMachineController.GetCoinAccepter();
+            coinAccepter.AcceptCoin(CoinAccepter.Coin.Dime);
+
+            bool waiting = true;
+            long startTime = System.DateTime.Now.Ticks;
+
+            while (waiting){
+
+                long currentTime = System.DateTime.Now.Ticks;
+                System.TimeSpan timeSpan = new System.TimeSpan(currentTime - startTime);
+                if (timeSpan.Seconds > 4){
+
+                    waiting = false;
+                }
+            }
+
+            DisplaySoldOutStrategy soldOutStrategy = new DisplaySoldOutStrategy(digitalDisplay);
+
+            string testDeposit = "$0.10";
+
+            Assert.AreEqual(testDeposit, digitalDisplay.GetCurrentMessage());
+        }
     }
 }
