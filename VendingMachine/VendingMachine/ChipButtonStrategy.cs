a1358bc432cdf1aa0b4a04cf8942563d685097ba
Michael Lezon <mister.catsby@gmail.com>
2016-03-10 12:47:28 -0500

    Added a test to the chip button strategy to verify correct output if the chips are out of stock.

--- VendingMachine/VendingMachine/ChipButtonStrategy.cs
+++ VendingMachine/VendingMachine/ChipButtonStrategy.cs
@@ -10,6 +10,20 @@ namespace VendingMachineNS{
 
             m_vendingMachineController = vendingMachineController;
 
+            m_vendingMachineController = vendingMachineController;
+
+            CoinAccepter coinAccepter = m_vendingMachineController.GetCoinAccepter();
+            decimal currentDeposit = coinAccepter.GetCurrentDeposit();
+
+            DigitalDisplay digitalDisplay = m_vendingMachineController.GetDigitalDisplay();
+            ProductDispenser productDispenser = m_vendingMachineController.GetProductDispenser();
+            SnackBox snackBox = m_vendingMachineController.GetSnackBox();
+            int productStock = snackBox.GetProductStock("Chips");
+
+            if (productStock == 0){
+
+                digitalDisplay.UserSelectedASoldOutProduct();
+            }
         }
 
         public VendingMachineController GetVendingMachineController(){
--- VendingMachine/VendingMachineTest/ChipButtonStrategyTests.cs
+++ VendingMachine/VendingMachineTest/ChipButtonStrategyTests.cs
@@ -9,13 +9,31 @@ namespace VendingMachineTestNS {
         VendingMachineController testVendingMachineController;
 
         [TestMethod]
-        public void TestChipButtonStrategyReferenceIsCorrect(){
+        public void TestChipButtonStrategyReferenceToVendingMachineControllerIsCorrect(){
 
             testVendingMachineController = new VendingMachineController();
             decimal testProductPrice = .5m;
             ChipButtonStrategy chipButtonStrategy = new ChipButtonStrategy(testVendingMachineController, testProductPrice);
 
-            Assert.AreEqual(testVendingMachineController,chipButtonStrategy.GetVendingMachineController());
+            Assert.AreEqual(testVendingMachineController, chipButtonStrategy.GetVendingMachineController());
+
+        }
+
+        [TestMethod]
+        public void TestStrategyHasProperOutcomeIfProductIsOutOfStock(){
+
+            testVendingMachineController = new VendingMachineController();
+            SnackBox snackBox = testVendingMachineController.GetSnackBox();
+            snackBox.SetProductStock("Chips", 0);
+
+            decimal testProductPrice = 2;
+            ChipButtonStrategy chipButtonStrategy = new ChipButtonStrategy(testVendingMachineController, testProductPrice);
+
+            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
+
+            string testDisplayOutput = "SOLD OUT";
+
+            Assert.AreEqual(testDisplayOutput, digitalDisplay.DisplayMessage());
 
         }
     }
