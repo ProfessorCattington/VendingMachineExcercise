5959b5f9e45616fe794f1a2a9edc76f2dbd4a5e1
Michael Lezon <mister.catsby@gmail.com>
2016-03-10 12:35:32 -0500

    added a new test class for a new chip button strategy and a basic test for the chip button strategy;

---
+++ VendingMachine/VendingMachine/ChipButtonStrategy.cs
@@ -0,0 +1,20 @@
+﻿using VendingMachineNS;
+
+namespace VendingMachineNS{
+
+    public class ChipButtonStrategy{
+
+        private VendingMachineController m_vendingMachineController;
+
+        public ChipButtonStrategy(VendingMachineController vendingMachineController, decimal productPrice){
+
+            m_vendingMachineController = vendingMachineController;
+
+        }
+
+        public VendingMachineController GetVendingMachineController(){
+
+            return m_vendingMachineController;
+        }
+    }
+}
---
+++ VendingMachine/VendingMachineTest/ChipButtonStrategyTests.cs
@@ -0,0 +1,22 @@
+﻿using VendingMachineNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTestNS { 
+
+    [TestClass]
+    public class ChipButtonStrategyTests{
+
+        VendingMachineController testVendingMachineController;
+
+        [TestMethod]
+        public void TestChipButtonStrategyReferenceIsCorrect(){
+
+            testVendingMachineController = new VendingMachineController();
+            decimal testProductPrice = .5m;
+            ChipButtonStrategy chipButtonStrategy = new ChipButtonStrategy(testVendingMachineController, testProductPrice);
+
+            Assert.AreEqual(testVendingMachineController,chipButtonStrategy.GetVendingMachineController());
+
+        }
+    }
+}
