f4a5001ca746b12f34031ee8b6b445cf11bd5aa1
Michael Lezon <mister.catsby@gmail.com>
2016-03-10 15:22:39 -0500

    added a test to make sure the strategy changes the display state after a short wait.

--- VendingMachine/VendingMachineTest/DisplayPriceStrategyTests.cs
+++ VendingMachine/VendingMachineTest/DisplayPriceStrategyTests.cs
@@ -21,5 +21,32 @@ namespace VendingMachineTestNS {
 
             Assert.AreEqual("PRICE " + testPrice, digitalDisplay.GetCurrentMessage());
         }
+
+        [TestMethod]
+        public void TestDisplayStrategyChangesDisplayAfterAShortWait(){
+
+            testVendingMachineController = new VendingMachineController();
+
+            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
+            string testPrice = "$0.99";
+            digitalDisplay.UserHasntDepositedEnough(testPrice);
+
+            bool waiting = true;
+            long startTime = System.DateTime.Now.Ticks;
+
+            while (waiting){
+
+                long currentTime = System.DateTime.Now.Ticks;
+                System.TimeSpan timeSpan = new System.TimeSpan(currentTime - startTime);
+                if (timeSpan.Seconds > 4){
+
+                    waiting = false;
+                }
+            }
+
+            DisplayPriceStrategy displayPriceStrategy = new DisplayPriceStrategy(digitalDisplay);
+
+            Assert.AreEqual("INSERT COINS", digitalDisplay.GetCurrentMessage());
+        }
     }
 }
