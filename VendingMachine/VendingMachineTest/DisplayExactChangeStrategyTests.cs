be6e6894eddb1b76a8c646443d9c6d6dec74f4a4
Michael Lezon <mister.catsby@gmail.com>
2016-03-10 16:11:33 -0500

    added a display exact change only strategy and testing class. testing class has a test for making sure the strategy doesn't immediately blow away the exact change only notification.

---
+++ VendingMachine/VendingMachineTest/DisplayExactChangeStrategyTests.cs
@@ -0,0 +1,26 @@
+﻿using VendingMachineNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTestNS { 
+
+    [TestClass]
+    public class DisplayExactChangeStrategyTests{
+
+        private VendingMachineController testVendingMachineController;
+
+        [TestMethod]
+        public void TestDisplayStrategyDoesntImmediatelyRemoveExactChangeNotification(){
+
+            testVendingMachineController = new VendingMachineController();
+
+            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
+            digitalDisplay.UserSelectedExactChangeOnlyProduct();
+
+            DisplayExactChangeStrategy exactChangeStrategy = new DisplayExactChangeStrategy(digitalDisplay);
+
+            string testExactChange = "EXACT CHANGE ONLY";
+
+            Assert.AreEqual(testExactChange, digitalDisplay.GetCurrentMessage());
+        }
+    }
+}
