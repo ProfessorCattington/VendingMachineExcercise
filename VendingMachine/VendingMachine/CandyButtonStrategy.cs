db86c851192dbd4d8c3c409d7b60ccdfba94b30a
Michael Lezon <mister.catsby@gmail.com>
2016-03-10 11:45:57 -0500

    beginning cleanup work on the control panel class by moving the button press logic into strategy classes. added a basic test for a candy button strategy.

---
+++ VendingMachine/VendingMachine/CandyButtonStrategy.cs
@@ -0,0 +1,18 @@
+﻿using VendingMachineNS;
+
+namespace VendingMachineNS { }
+
+    public class CandyButtonStrategy{
+
+    private VendingMachineController m_vendingMachineController; 
+
+    public CandyButtonStrategy(VendingMachineController vendingMachineController){
+
+        m_vendingMachineController = vendingMachineController;
+    }
+
+    public VendingMachineController GetVendingMachineController(){
+
+        return m_vendingMachineController;
+    }
+}
---
+++ VendingMachine/VendingMachineTest/CandyButtonStrategyTests.cs
@@ -0,0 +1,21 @@
+﻿using VendingMachineNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTestNS{
+
+    [TestClass]
+    public class CandyButtonStrategyTests{
+
+        VendingMachineController testVendingMachineController;
+
+        [TestMethod]
+        public void TestButtonPressStrategyReferenceToVendingMachineController(){
+
+            testVendingMachineController = new VendingMachineController();
+
+            CandyButtonStrategy candyButtonStrategy = new CandyButtonStrategy(testVendingMachineController);
+
+            Assert.AreEqual(testVendingMachineController, candyButtonStrategy.GetVendingMachineController());
+        }
+    }
+}
