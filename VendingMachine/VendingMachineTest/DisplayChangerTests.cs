16af199560956b34f059321619144cfa23d155b9
Michael Lezon <mister.catsby@gmail.com>
2016-03-29 19:31:16 -0400

    Added a new test class and basic test. New class will replace all of the display handling strategies' behaviours in order to make the digital display class clearer and easier to read while preventing the project from expanding into too many semi redundant class files.

---
+++ VendingMachine/VendingMachineTest/DisplayChangerTests.cs
@@ -0,0 +1,26 @@
+﻿using VendingMachineNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTestNS{
+
+    [TestClass]
+    public class DisplayChangerTests{
+
+        VendingMachineController testVendingMachineController;
+
+        [TestMethod]
+        public void TestDisplayStrategyDoesntImmediatelyRemoveExactChangeNotification(){
+
+            testVendingMachineController = new VendingMachineController();
+
+            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
+            digitalDisplay.UserSelectedExactChangeOnlyProduct();
+
+            DisplayChanger displayChanger = new DisplayChanger(digitalDisplay);
+
+            string testExactChange = "EXACT CHANGE ONLY";
+
+            Assert.AreEqual(testExactChange, digitalDisplay.GetCurrentMessage());
+        }
+    }
+}
