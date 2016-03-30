eec587713429c915202de0bcdc9cf62aac5d875e
Michael Lezon <mister.catsby@gmail.com>
2016-03-29 20:23:11 -0400

    I mistakenly copied too much code from the old displayexactstrategy class into the displaychanger class for the previous test. I think this commit's test will cover for the appropriate exact change only conditions.

--- VendingMachine/VendingMachineTest/DisplayChangerTests.cs
+++ VendingMachine/VendingMachineTest/DisplayChangerTests.cs
@@ -35,5 +35,36 @@ namespace VendingMachineTestNS{
 
             Assert.AreEqual(testExactChange, digitalDisplay.GetCurrentMessage());
         }
+
+        [TestMethod]
+        public void TestDisplayChangerChangesMessageAfterAWaitWithDepositOnExactChangerPurchase(){
+
+            testVendingMachineController = new VendingMachineController();
+
+            CoinAccepter coinAccepter = testVendingMachineController.GetCoinAccepter();
+            coinAccepter.AcceptCoin(CoinAccepter.Coin.Quarter);
+
+            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
+            digitalDisplay.UserSelectedExactChangeOnlyProduct();
+
+            bool waiting = true;
+            long startTime = System.DateTime.Now.Ticks;
+
+            while (waiting){
+
+                long currentTime = System.DateTime.Now.Ticks;
+                System.TimeSpan timeSpan = new System.TimeSpan(currentTime - startTime);
+                if (timeSpan.Seconds > 4){
+
+                    waiting = false;
+                }
+            }
+
+            DisplayExactChangeStrategy exactChangeStrategy = new DisplayExactChangeStrategy(digitalDisplay);
+
+            string testExactChange = "$0.25";
+
+            Assert.AreEqual(testExactChange, digitalDisplay.GetCurrentMessage());
+        }
     }
 }
