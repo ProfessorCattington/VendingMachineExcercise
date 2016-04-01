c04301e4d241fc46a9a046ce781da3f27751dd67
Michael Lezon <mister.catsby@gmail.com>
2016-04-01 17:32:55 -0400

    rewriting how products are set and retreived by the snackbox and controlpanel in order to remove the button strategies. first new test will just make sure the snackbox constructs itself properly.

--- VendingMachine/VendingMachineTest/SnackBoxTests.cs
+++ VendingMachine/VendingMachineTest/SnackBoxTests.cs
@@ -9,101 +9,19 @@ namespace VendingMachineTest
         VendingMachineController testVendingMachineController;
 
         [TestMethod]
-        public void TestVendingMachineDisplayNotifiesUserWhenProductIsSoldOut(){
+        void TestSnackBoxConstructorsProperly(){
 
             testVendingMachineController = new VendingMachineController();
-
             SnackBox snackBox = testVendingMachineController.GetSnackBox();
-            snackBox.SetProductStock("Cola", 0);
 
-            ControlPanel controlPanel = testVendingMachineController.GetControlPanel();
-            controlPanel.UserPushedAButton(ControlPanel.buttons.cola);
-
-            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
-
-            string testDisplayOutput = "SOLD OUT";
-
-            Assert.AreEqual(testDisplayOutput, digitalDisplay.DisplayMessage());
-
+            Assert.IsNotNull(snackBox);
+            Assert.IsNotNull(snackBox.GetProducts());
         }
 
         [TestMethod]
-        public void TestSoldOutMessageDoesntPersistWithoutDeposit(){
+        void TestProductListSetterSetsTheCorrectProduct()
+        {
 
-            testVendingMachineController = new VendingMachineController();
-
-            SnackBox snackBox = testVendingMachineController.GetSnackBox();
-            snackBox.SetProductStock("Cola", 0);
-
-            ControlPanel controlPanel = testVendingMachineController.GetControlPanel();
-            controlPanel.UserPushedAButton(ControlPanel.buttons.cola);
-
-            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
-
-            System.DateTime purchaseTime = System.DateTime.Now;
-            System.DateTime currentTime;
-
-            bool waiting = true;
-            while (waiting)
-            {
-
-                currentTime = System.DateTime.Now;
-                long elapsedTime = currentTime.Ticks - purchaseTime.Ticks;
-
-                System.TimeSpan timeSpan = new System.TimeSpan(elapsedTime);
-
-                if (timeSpan.Seconds > 4)
-                {
-
-                    waiting = false;
-                }
-            }
-
-            string testDisplayOutput = "INSERT COINS";
-
-            Assert.AreEqual(testDisplayOutput, digitalDisplay.DisplayMessage());
-
-        }
-
-        [TestMethod]
-        public void TestSoldOutMessageDoesntPersistWithDeposit(){
-
-            testVendingMachineController = new VendingMachineController();
-
-            SnackBox snackBox = testVendingMachineController.GetSnackBox();
-            snackBox.SetProductStock("Cola", 0);
-
-            CoinAccepter coinAccepter = testVendingMachineController.GetCoinAccepter();
-            coinAccepter.AcceptCoin(CoinAccepter.Coin.Quarter);
-            coinAccepter.AcceptCoin(CoinAccepter.Coin.Quarter);
-
-            ControlPanel controlPanel = testVendingMachineController.GetControlPanel();
-            controlPanel.UserPushedAButton(ControlPanel.buttons.cola);
-
-            DigitalDisplay digitalDisplay = testVendingMachineController.GetDigitalDisplay();
-
-            System.DateTime purchaseTime = System.DateTime.Now;
-            System.DateTime currentTime;
-
-            bool waiting = true;
-            while (waiting)
-            {
-
-                currentTime = System.DateTime.Now;
-                long elapsedTime = currentTime.Ticks - purchaseTime.Ticks;
-
-                System.TimeSpan timeSpan = new System.TimeSpan(elapsedTime);
-
-                if (timeSpan.Seconds > 4)
-                {
-
-                    waiting = false;
-                }
-            }
-
-            string testDisplayOutput = "$0.50";
-
-            Assert.AreEqual(testDisplayOutput, digitalDisplay.DisplayMessage());
         }
     }
 }
