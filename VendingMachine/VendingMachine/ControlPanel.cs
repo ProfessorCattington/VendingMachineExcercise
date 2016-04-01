7dead392bf73b75b8fd629f1cbca893221a4d237
Michael Lezon <mister.catsby@gmail.com>
2016-04-01 18:52:03 -0400

    excluded the unused strategies and their tests from the project.

--- VendingMachine/VendingMachine/ControlPanel.cs
+++ VendingMachine/VendingMachine/ControlPanel.cs
@@ -3,18 +3,6 @@ namespace VendingMachineNS {
 
     public class ControlPanel{
 
-        private const decimal m_candyPrice = 0.65m;
-        private const decimal m_colaPrice = 1.0m;
-        private const decimal m_chipsPrice = 0.5m;
-
-        public enum buttons{
-
-            candy,
-            chip,
-            cola,
-            coinReturn
-        }
-
         private VendingMachineController m_vendingMachineController;
 
         public ControlPanel(VendingMachineController vendingMachineController) {
@@ -73,36 +61,7 @@ namespace VendingMachineNS {
             ProductDispenser productDispenser = m_vendingMachineController.GetProductDispenser();
             productDispenser.SetLastProductDispensed("None");
         }
-        //switch (button){
-
-        //    case buttons.candy:
-
-        //        new CandyButtonStrategy(m_vendingMachineController, m_candyPrice);
-
-        //        break;
-
-        //    case buttons.chip:
-
-        //        new ChipButtonStrategy(m_vendingMachineController, m_chipsPrice);
-
-        //        break;
-
-        //    case buttons.cola:
-
-        //        new ColaButtonStrategy(m_vendingMachineController, m_colaPrice);
-
-        //        break;
-
-        //    case buttons.coinReturn:
-
-        //        new CoinReturnButtonStrategy(m_vendingMachineController);
-
-        //        break;
-
-        //    default:
-        //        break;
-
-        //}
+      
         public VendingMachineController GetVendingMachineController(){
 
             return m_vendingMachineController;
