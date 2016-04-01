29fb5df849705818a9e0a05925081ced306d16d8
Michael Lezon <mister.catsby@gmail.com>
2016-04-01 17:46:11 -0400

    added a test class for the product class. single test added to make sure that product objects are constructed properly.

---
+++ VendingMachine/VendingMachineTest/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachineNS;
+
+namespace VendingMachineTestNS{
+
+    [TestClass]
+    public class ProductTests{
+
+        [TestMethod]
+        public void TestProductConstructsProperly(){
+
+            string testName = "CandyBar";
+            decimal testPrice = .50m;
+            int testStock = 10;
+
+            Product candyBar = new Product(testName, testPrice,testStock);
+
+            Assert.AreEqual(testName, candyBar.GetName());
+            Assert.AreEqual(testPrice, candyBar.GetPrice());
+            Assert.AreEqual(testStock, candyBar.GetStock());
+        }
+    }
+}
